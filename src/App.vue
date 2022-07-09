e03ea4728d7b6f6cc456eb7aee85ba3d38a9101f
cyubahiro367 <theotime.c@kudibooks.com>
2022-07-09 08:35:41 +0200

    created sponsor component and import it in app component

--- src/App.vue
+++ src/App.vue
@@ -450,156 +450,14 @@
       <Venue />
       <!--//venue-section-->
 
-      <section id="sponsors-section" class="sponsors-section section">
-        <div class="container">
-          <h3 class="section-heading text-center mb-3">
-            Sponsors &amp; Partners
-          </h3>
-          <div class="section-intro text-center single-col-max mx-auto mb-5">
-            Want to become a sponsor? Get in touch lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Proin consequat magna eu accumsan
-            mattis.
-          </div>
-          <div class="row logos justify-content-center">
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-1.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-2.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-3.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-4.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-5.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-6.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-7.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-8.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-9.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-10.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-11.svg" alt="" />
-            </div>
-            <div class="logo-item col-6 col-md-4 col-lg-2">
-              <img src="assets/images/logos/logo-12.svg" alt="" />
-            </div>
-          </div>
-          <!--//row-->
-          <div class="sponsors-cta text-center pt-5">
-            <a class="btn-primary btn btn-lg" href="#">Become A Sponsor</a>
-          </div>
-        </div>
-        <!--//container-->
-      </section>
+      <Sponsor />
       <!--//sponsors-section-->
 
       <Footer />
       <!--//Footer-->
 
       <!-- Modal Speaker -->
-      <div
-        class="modal modal-speaker modal-speaker-1"
-        id="modal-speaker-1"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="speaker-1-ModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-hidden="true"
-              >
-                &times;
-              </button>
-              <h4 id="speaker-1-ModalLabel" class="modal-title sr-only">
-                Speaker Name
-              </h4>
-            </div>
-            <div class="modal-body p-0">
-              <div class="theme-bg-light p-5">
-                <div class="row">
-                  <div class="col-12 col-md-auto text-center">
-                    <img
-                      class="
-                        profile-image
-                        mb-3 mb-md-0
-                        me-md-4
-                        rounded-circle
-                        mx-auto
-                      "
-                      src="assets/images/speakers/speaker-1.jpg"
-                      alt=""
-                    />
-                  </div>
-
-                  <div class="col text-center text-md-start mx-auto">
-                    <h2 class="name mb-2">Speaker Name</h2>
-                    <div class="meta">Job Title Or Position</div>
-                    <div class="meta mb-2">Company Or Organization</div>
-                    <ul class="social-list list-inline mb-0">
-                      <li class="list-inline-item">
-                        <a href="#"><i class="fab fa-twitter fa-fw"></i></a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a>
-                      </li>
-                      <li class="list-inline-item">
-                        <a href="#"><i class="fab fa-github fa-fw"></i></a>
-                      </li>
-                    </ul>
-                    <!--//social-list-->
-                  </div>
-                  <!--//col-->
-                </div>
-                <!--//row-->
-              </div>
-              <div class="desc p-4 p-lg-5">
-                <p>
-                  You can put speaker's bio or talk related info here. Duis a mi
-                  quis metus porttitor eleifend. Pellentesque finibus ultrices
-                  imperdiet. Maecenas auctor tortor quis risus tincidunt, mattis
-                  mattis leo placerat. Fusce metus augue, sagittis eget enim
-                  vel, venenatis auctor est. In interdum felis massa, ac porta
-                  nunc pretium non. In fringilla orci vitae imperdiet malesuada.
-                  Vestibulum feugiat lobortis est, in sagittis nisi molestie
-                  vel. Mauris ultrices vitae lectus eu feugiat. Fusce semper,
-                  nisi at placerat mollis, augue elit pretium enim, eu
-                  pellentesque justo purus et lectus.
-                </p>
-                <p class="mb-0">
-                  Donec neque magna, molestie vel varius ut, pretium a urna.
-                  Pellentesque placerat nunc eu condimentum pellentesque.
-                  Vivamus dictum nisl leo, id fermentum lectus porttitor et.
-                  Pellentesque tristique erat libero, condimentum porttitor nisl
-                  pharetra et.
-                </p>
-              </div>
-            </div>
-            <!--//modal-body-->
-          </div>
-          <!--//modal-content-->
-        </div>
-        <!--//modal-dialog-->
-      </div>
+      
       <!--//modal-->
     </div>
     <router-view />
@@ -610,12 +468,14 @@
 import Speakers from './components/Speakers.vue'
 import Schedule from './components/Schedule.vue'
 import Venue from './components/Venue.vue'
+import Sponsor from './components/Sponsor.vue'
 import Footer from './components/Footer.vue'
 export default {
   components:{
     Speakers,
     Schedule,
     Venue,
+    Sponsor,
     Footer
   }
 }
