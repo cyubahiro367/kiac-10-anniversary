7097fd96a65549f5e3159e2a11f0ae2f23d0a02c
cyubahiro367 <theotime.c@kudibooks.com>
2022-08-18 16:31:18 +0200

    refactored registration form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,24 +10,14 @@
             <div class="col-md-6 col-sm-12 col-xs-12">
               <label>TITLE NAMES <span style="color: red">*</span></label>
               <div class="row">
-                <div class="col-md-2 col-sm-12 col-xs-12">
-                  <select class="form-control" name="cars" id="cars">
-                    <option value="volvo">Mr</option>
-                    <option value="saab">Mrs</option>
-                    <option value="mercedes">Ms</option>
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                  <select v-model="attender.title" class="form-control" name="cars" id="cars">
+                    <option value="Mr">Mr</option>
+                    <option value="Mrs">Mrs</option>
+                    <option value="Ms">Ms</option>
                   </select>
-
-                  <!-- <input
-                    name="firstName"
-                    type="text"
-                    class="form-control"
-                    id="fullname"
-                    placeholder="First Name"
-                    v-model="attender.firstName"
-                    required
-                  /> -->
                 </div>
-                <div class="col-md-5 col-sm-12 col-xs-12">
+                <div class="col-md-9 col-sm-12 col-xs-12">
                   <input
                     name="firstName"
                     type="text"
@@ -35,17 +25,6 @@
                     id="fullname"
                     placeholder="First Name"
                     v-model="attender.firstName"
-                    required
-                  />
-                </div>
-                <div class="col-md-5 col-sm-12 col-xs-12">
-                  <input
-                    name="lastName"
-                    type="text"
-                    class="form-control"
-                    id="fullname"
-                    placeholder="Last Name"
-                    v-model="attender.lastName"
                     required
                   />
                 </div>
@@ -53,14 +32,14 @@
             </div>
 
             <div class="col-md-6 col-sm-12 col-xs-12">
-              <label>EMAIL <span style="color: red">*</span></label>
+              <label>LAST NAME <span style="color: red">*</span></label>
               <input
-                name="email"
-                type="email"
+                name="lastName"
+                type="lastName"
                 class="form-control"
-                id="email"
-                placeholder="example@example.com"
-                v-model="attender.email"
+                id="lastName"
+                placeholder="Enter your last name"
+                v-model="attender.lastName"
                 required
               />
             </div>
@@ -68,14 +47,14 @@
           <br />
           <div class="row">
             <div class="col-md-6 col-sm-12 col-xs-12">
-              <label>ORGANISATION <span style="color: red">*</span></label>
+              <label>EMAIL <span style="color: red">*</span></label>
               <input
-                name="organisation"
-                type="text"
+                name="email"
+                type="email"
                 class="form-control"
-                id="fullname"
-                placeholder="organisation"
-                v-model="attender.organisation"
+                id="email"
+                placeholder="email"
+                v-model="attender.email"
                 required
               />
             </div>
@@ -94,16 +73,34 @@
             </div>
           </div>
           <br />
-          <label>COMPANY ADDRESS <span style="color: red">*</span></label>
-          <input
-            name="companyAddress"
-            type="text"
-            class="form-control"
-            id="fullname"
-            placeholder="Company Address"
-            v-model="attender.companyAddress"
-            required
-          />
+
+          <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+              <label>ORGANISATION <span style="color: red">*</span></label>
+              <input
+                name="organisation"
+                type="text"
+                class="form-control"
+                id="fullname"
+                placeholder="organisation"
+                v-model="attender.organisation"
+                required
+              />
+            </div>
+
+            <div class="col-md-6 col-sm-12 col-xs-12">
+              <label>COMPANY ADDRESS <span style="color: red">*</span></label>
+              <input
+                name="companyAddress"
+                type="text"
+                class="form-control"
+                id="CompanyAddress"
+                placeholder="Company Address"
+                v-model="attender.companyAddress"
+                required
+              />
+            </div>
+          </div>
           <br />
           <div class="row">
             <div class="col-md-6 col-sm-12 col-xs-12">
@@ -173,6 +170,7 @@ export default {
   data() {
     return {
       attender: {
+        title: null,
         firstName: null,
         lastName: null,
         email: null,
@@ -191,7 +189,7 @@ export default {
       this.$Progress.start();
 
       const data = {
-        firstName: this.attender.firstName,
+        firstName: this.attender.title + " " + this.attender.firstName,
         lastName: this.attender.lastName,
         email: this.attender.email,
         organisation: this.attender.organisation,
@@ -203,9 +201,9 @@ export default {
 
       try {
         await axios.post(`/api/attender`, data);
-        this.$Progress.finish();
         this.$noty.success("you have been registered to attend kiac 100 anniversary");
         this.$router.push({ name: "ThankYou" });
+        this.$Progress.finish();
       } catch (error) {
         this.$Progress.fail();
         this.$noty.error("failed contact support");
