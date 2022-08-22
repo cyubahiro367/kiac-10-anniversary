7cc5638d37dac8a13cc899860e687ff2275b5338
cyubahiro367 <theotime.c@kudibooks.com>
2022-08-22 07:44:24 +0200

    added new sponsors

--- src/components/Sponsor.vue
+++ src/components/Sponsor.vue
@@ -2,9 +2,7 @@
   <div>
     <section id="sponsors-section" class="sponsors-section section">
       <div class="container">
-        <h3 class="section-heading text-center mb-3">
-          STRATEGIC PARTNERS
-        </h3>
+        <h3 class="section-heading text-center mb-3">STRATEGIC PARTNERS</h3>
         <div class="section-intro text-center single-col-max mx-auto mb-5">
           <p>
             The event will provide a golden marketing opportunity to showcase
@@ -55,6 +53,12 @@
           <div class="logo-item col-6 col-md-4 col-lg-3">
             <img src="assets/images/logos/11.jpg" alt="" />
           </div>
+          <div class="logo-item col-6 col-md-4 col-lg-3">
+            <img src="assets/images/logos/12.jpg" alt="" />
+          </div>
+          <div class="logo-item col-6 col-md-4 col-lg-3">
+            <img src="assets/images/logos/13.jpg" alt="" />
+          </div>
         </div>
         <!--//row-->
         <div class="sponsors-cta text-center pt-5">
@@ -65,89 +69,5 @@
       </div>
       <!--//container-->
     </section>
-
-    <!-- Modal Main Hotels -->
-    <!-- <div
-      class="modal modal-speaker modal-speaker-1"
-      id="modal-speaker-1"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="speaker-1-ModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-hidden="true"
-            >
-              &times;
-            </button>
-            <h4 id="speaker-1-ModalLabel" class="modal-title sr-only">
-              Speaker Name
-            </h4>
-          </div>
-          <div class="modal-body p-0">
-            <div class="theme-bg-light p-5">
-              <div class="row">
-                <div class="col-12 col-md-auto text-center">
-                  <img
-                    class="
-                      profile-image
-                      mb-3 mb-md-0
-                      me-md-4
-                      rounded-circle
-                      mx-auto
-                    "
-                    src="assets/images/speakers/speaker-1.jpg"
-                    alt=""
-                  />
-                </div>
-
-                <div class="col text-center text-md-start mx-auto">
-                  <h2 class="name mb-2">Speaker Name</h2>
-                  <div class="meta">Job Title Or Position</div>
-                  <div class="meta mb-2">Company Or Organization</div>
-                  <ul class="social-list list-inline mb-0">
-                    <li class="list-inline-item">
-                      <a href="#"><i class="fab fa-twitter fa-fw"></i></a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a>
-                    </li>
-                    <li class="list-inline-item">
-                      <a href="#"><i class="fab fa-github fa-fw"></i></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="desc p-4 p-lg-5">
-              <p>
-                You can put speaker's bio or talk related info here. Duis a mi
-                quis metus porttitor eleifend. Pellentesque finibus ultrices
-                imperdiet. Maecenas auctor tortor quis risus tincidunt, mattis
-                mattis leo placerat. Fusce metus augue, sagittis eget enim vel,
-                venenatis auctor est. In interdum felis massa, ac porta nunc
-                pretium non. In fringilla orci vitae imperdiet malesuada.
-                Vestibulum feugiat lobortis est, in sagittis nisi molestie vel.
-                Mauris ultrices vitae lectus eu feugiat. Fusce semper, nisi at
-                placerat mollis, augue elit pretium enim, eu pellentesque justo
-                purus et lectus.
-              </p>
-              <p class="mb-0">
-                Donec neque magna, molestie vel varius ut, pretium a urna.
-                Pellentesque placerat nunc eu condimentum pellentesque. Vivamus
-                dictum nisl leo, id fermentum lectus porttitor et. Pellentesque
-                tristique erat libero, condimentum porttitor nisl pharetra et.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
   </div>
 </template>
